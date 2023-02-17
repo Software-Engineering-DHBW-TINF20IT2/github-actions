8a5f7873aa39b5cfda11d4f2e06f1ff7910237c6
Marco Dörfler <marco.doerfler@sophistex.com>
2023-02-17 09:44:01 +0100

    Initialize gh pages

--- src/backend/run.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-print("Running Backend...")
-print("Python version is: " + sys.version)
-
-sys.exit(0)
-
--- src/frontend/app.js
+++
@@ -1,3 +0,0 @@
-console.log('Running app...');
-console.log('Node.js version is: ' + process.version)
-
